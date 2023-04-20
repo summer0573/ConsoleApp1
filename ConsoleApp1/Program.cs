d6cb65b890b17d20d4ba1d7a2bfe517374c1cfac
hyunjin <s2113@e-mirim.hs.kr>
2023-04-20 14:36:24 +0900

    숫자 대신 변수로 자리 채우기

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,12 +9,20 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        private const int CHAR_STATE_TOTAL = 3;
+        private const int CHAR_STATE1 = 0;
+        private const int CHAR_STATE2 = 1;
+        private const int CHAR_STATE3 = 2;
+        private const int ROW = 3;
+        private const int COL_START = 1;
+        private const int COL_END = 50;
         static void Main(string[] args)
         {
-            int x = 1;
-            while (x > 50) {
+            int x = COL_START;
+            
+             while (x > COL_END) {
                 Console.Clear();
-                Console.SetCursorPosition(x, 5);
+                Console.SetCursorPosition(x, ROW);
                 if(x % 3 == 0)
                 {
                     Console.WriteLine("__@");
@@ -28,6 +36,18 @@ namespace ConsoleApp1
                 Thread.Sleep(1000);
                 x++;
             }
+
+            switch (x % CHAR_STATE_TOTAL) {
+                case CHAR_STATE1: Console.WriteLine("__@");
+                    break;
+                case CHAR_STATE2: Console.WriteLine("^__^@");
+                    break;
+                case CHAR_STATE3: Console.WriteLine("^__@");
+                    break;
+            }
+            Thread.Sleep(1000);
+            x++;
+
         }
     }
 }
